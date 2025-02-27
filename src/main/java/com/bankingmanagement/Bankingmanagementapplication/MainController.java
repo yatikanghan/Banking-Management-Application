ed3967108ae22756c711b72583cb868f3756bfb4
yatikanghan <yatikanghan883@gmail.com>
2025-02-27 22:13:22 +0100

    register page

--- src/main/java/com/bankingmanagement/Bankingmanagementapplication/MainController.java
+++ src/main/java/com/bankingmanagement/Bankingmanagementapplication/MainController.java
@@ -1,7 +1,10 @@
 package com.bankingmanagement.Bankingmanagementapplication;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -19,4 +22,11 @@ public class MainController {
     public String register() {
         return "/customerregister";
     }
+
+    @PostMapping("/registeraccount")
+    public String registeraccount(Model model, @RequestParam String txtfname, @RequestParam String txtlname, @RequestParam String txtemail , @RequestParam String txtpassword, @RequestParam String txtmobilenumber , @RequestParam String txtaddress, @RequestParam String txtdob, @RequestParam String txtpostcode, @RequestParam String txtactype , @RequestParam String txtcountry) {
+        String substring= txtfname + " : " + txtlname + " : " + txtemail + " : " + txtpassword + " : " + txtmobilenumber + " : " + txtaddress + " : " + txtdob + " : " + txtpostcode + " : " + txtactype + " : " + txtcountry;
+        return "redirect:/";
+    }
+
 }
